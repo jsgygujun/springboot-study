3a9cb01a2ffd7c101a21e38cf29f01ef6cad495b
jsgygujun <jsgygujun@gmail.com>
2020-11-16 18:41:51 +0800

    [ADD] Configuration.

---
+++ springboot-config/src/main/java/com/jsgygujun/springbootconfig/config/MyConfiguration.java
@@ -0,0 +1,12 @@
+package com.jsgygujun.springbootconfig.config;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author GuJun
+ * @date 2020/11/16
+ */
+@Component
+public class MyConfiguration {
+
+}
